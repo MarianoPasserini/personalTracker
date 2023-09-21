e7f1720e1150f7164510ceedba91c553c06a1eec
Mariano Passerini <renacer258@gmail.com>
2023-09-21 17:12:46 -0300

    calendar changing according year

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const months = document.querySelectorAll('.month-item'); // Add a dot before REMEMBER!!!!
 const monthGrid = document.querySelector('.month-grid');
 const daysList = document.querySelectorAll('.days-list');
+const daysName = document.querySelectorAll('.days-name-li');
+const year = document.querySelector('.year');
 
 let currentMonth = -1; // variable to know if a month is active or not
 
@@ -19,6 +21,7 @@ months.forEach((month, index) => { // Adding an event listener to each month
             monthGrid.classList = 'month-grid-active';
             month.classList = 'month-item-active';
             daysList[index].classList = 'days-list-active';
+            daysName[index].classList = 'days-name-li-active';
         }
     });
 });
@@ -27,6 +30,7 @@ const resetView = () => { // Function to reset the view of the calendar
     months.forEach((month) => { // Set everything back to normal
         month.classList = 'month-item';
         daysList[currentMonth].classList = 'days-list';
+        daysName[currentMonth].classList = 'days-name-li';
     });
     monthGrid.classList = 'month-grid';
     currentMonth = -1; // reset our tracker
@@ -35,13 +39,12 @@ const resetView = () => { // Function to reset the view of the calendar
 const monthsOfCalendar = [...Array(12).keys()]; // Array of 12 months
 let actualYear = 2023 // Year to be displayed
 
-const intl = new Intl.DateTimeFormat("en-US", {month: 'long'})
+const intl = new Intl.DateTimeFormat("es-ES", {month: 'long'})
 
 const calendar = monthsOfCalendar.map(monthKey => {
-  const monthName = intl.format(new Date( actualYear, monthKey))
   return {
     daysOfMonth: new Date(actualYear, monthKey + 1, 0).getDate(),
-    startsOn: new Date(actualYear, monthKey, 1).getDay()
+    startsOn: new Date(actualYear, monthKey, 0).getDay()
   }
 })
 
@@ -54,10 +57,56 @@ const html = calendar.map(({daysOfMonth}) => {
 `
 })
 
+
 document.querySelectorAll('.days-list').forEach((list, index) => {
-    list.innerHTML = html[index]
-    })
+    list.innerHTML = html[index]; // Set the HTML content first
+    const dayItems = list.querySelectorAll('li'); // Get all the li elements
 
+    // Apply grid-column-start to each li element
+    dayItems.forEach((dayItem, dayIndex) => {
+        dayItem.style.gridColumnStart = (dayIndex + 1 + calendar[index].startsOn) % 7 || 7;
+    });
+});
 
+const yearBefore = () => {
+    actualYear--;
+    document.querySelector('.year').innerHTML = actualYear;
+    updateCalendar();
+}
 
+const yearAfter = () => {
+    actualYear++;
+    document.querySelector('.year').innerHTML = actualYear;
+    updateCalendar();
+}
 
+//update calendar according to actualYear
+
+const updateCalendar = () => {
+    const calendar = monthsOfCalendar.map(monthKey => {
+        return {
+          daysOfMonth: new Date(actualYear, monthKey + 1, 0).getDate(),
+          startsOn: new Date(actualYear, monthKey, 0).getDay()
+        }
+      })
+      
+      const html = calendar.map(({daysOfMonth}) => {
+          const renderDays = [...Array(daysOfMonth).keys()].map(day => {
+              return `<li>${day + 1}</li>`
+          }).join('')
+        return `
+          ${renderDays}
+      `
+      })
+      
+      
+      document.querySelectorAll('.days-list').forEach((list, index) => {
+          list.innerHTML = html[index]; // Set the HTML content first
+          const dayItems = list.querySelectorAll('li'); // Get all the li elements
+      
+          // Apply grid-column-start to each li element
+          dayItems.forEach((dayItem, dayIndex) => {
+              dayItem.style.gridColumnStart = (dayIndex + 1 + calendar[index].startsOn) % 7 || 7;
+          });
+      });
+}
